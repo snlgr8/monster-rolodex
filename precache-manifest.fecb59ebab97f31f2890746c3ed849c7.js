6d83ef085d4e850f6e6285e0dd8622f839cbb73b
sonal.chaplot <sonal.chaplot@dieboldnixdorf.com>
2020-09-24 19:18:20 +0530

    Updates

---
+++ precache-manifest.fecb59ebab97f31f2890746c3ed849c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705a4c879929f6533df087e8db26f37c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0d134e0dc6d3ef1c7b27",
+    "url": "/monster-rolodex/static/css/main.fae13803.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d134e0dc6d3ef1c7b27",
+    "url": "/monster-rolodex/static/js/main.65f90ca2.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
